96a42d3fe1df0a008f8b75e7f02e3cdcaaf04d84
randarura <tataf732@gmail.com>
2023-11-25 15:42:18 +0900

    api-development

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,12 +1,21 @@
 import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 
 import prisma from '../../../../lib/prisma'
 
 // GET /api/pets
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const queryName = searchParams.get('name')
+  // findMany returns a list of records.
   const pets = await prisma.pet.findMany({
+    // sort by id ascending
     orderBy: { id: 'asc' },
-})
+    // select only id, name, imageUrl, and owner.name
+    select: { id: true, name: true, imageUrl: true, owner: { select: { name: true } } },
+    where: queryName ? { name: { contains: queryName } } : undefined,
+  })
+  // return Response with pets to json
   return NextResponse.json({ pets })
 }
 
